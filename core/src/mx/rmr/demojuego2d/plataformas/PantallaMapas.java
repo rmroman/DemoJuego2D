fe1787b22fea1ef910b439c2ca8a1a4981d0c0c5
Roberto Martínez <rmroman@itesm.mx>
2020-10-12 15:40:05 -0500

    Cómo manipular el mapa con programación.

--- core/src/mx/rmr/demojuego2d/plataformas/PantallaMapas.java
+++ core/src/mx/rmr/demojuego2d/plataformas/PantallaMapas.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
 
 import mx.rmr.demojuego2d.Juego;
 import mx.rmr.demojuego2d.Pantalla;
@@ -15,6 +17,9 @@ public class PantallaMapas extends Pantalla
     // Mapa
     private TiledMap mapa;
     private OrthogonalTiledMapRenderer rendererMapa;
+
+    // timer
+    private float timerBloque;
 
 
     public PantallaMapas(Juego juego) {
@@ -32,12 +37,20 @@ public class PantallaMapas extends Pantalla
         manager.finishLoading();
         mapa = manager.get("mapas/mapa_mario.tmx");
         rendererMapa = new OrthogonalTiledMapRenderer(mapa);
+
+        // Modificar el mapa
+        TiledMapTileLayer capa = (TiledMapTileLayer)mapa.getLayers().get(0);
+        TiledMapTileLayer.Cell celda = capa.getCell(0, 0);
+        for (int y = 1; y < capa.getHeight()/2; y++) {
+            capa.setCell(0, y, celda);
+        }
     }
 
     @Override
     public void render(float delta) {
         // Actualizar
-        actualizarCamara();
+        //actualizarCamara();
+        quitarBloques(delta);
 
         borrarPantalla(0,1,0);
 
@@ -45,6 +58,17 @@ public class PantallaMapas extends Pantalla
 
         rendererMapa.setView(camara);
         rendererMapa.render();
+    }
+
+    private void quitarBloques(float delta) {
+        timerBloque += delta;
+        if (timerBloque>0.3f) {
+            timerBloque = 0;
+            TiledMapTileLayer capa = (TiledMapTileLayer)mapa.getLayers().get(1);
+            int x = MathUtils.random(0, 40);
+            int y = MathUtils.random(0, 21);
+            capa.setCell(x, y, null);
+        }
     }
 
     private void actualizarCamara() {
