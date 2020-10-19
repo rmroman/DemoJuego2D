63522c265e332b3e3faced32bba0cba75acd8f50
Roberto Mtz. Román <rmroman@itesm.mx>
2020-10-19 16:16:34 -0500

    Botón en la pantalla pausa y retraso al inicio del juego

--- core/src/mx/rmr/demojuego2d/plataformas/PantallaMapas.java
+++ core/src/mx/rmr/demojuego2d/plataformas/PantallaMapas.java
@@ -5,18 +5,28 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import mx.rmr.demojuego2d.Alien;
+import mx.rmr.demojuego2d.BolaFuego;
 import mx.rmr.demojuego2d.Juego;
 import mx.rmr.demojuego2d.Pantalla;
 
@@ -30,8 +40,14 @@ public class PantallaMapas extends Pantalla
     private float timerBloque;
 
     // PAUSA
-    private EstadoJuego estado = EstadoJuego.JUGANDO;     // JUGANDO, PAUSADO, PIERDE, GANA
+    private EstadoJuego estado = EstadoJuego.INICIANDO;     // JUGANDO, PAUSADO, PIERDE, GANA
     private EscenaPausa escenaPausa;
+    // Cámara/Vista HUD
+    private OrthographicCamera camaraHUD;
+    private Viewport vistaHUD;
+
+    private float timerPausa;   // 3 seg
+
 
     public PantallaMapas(Juego juego) {
     }
@@ -39,8 +55,15 @@ public class PantallaMapas extends Pantalla
     @Override
     public void show() {
         crearMapa();
-
+        crearHUD();
         Gdx.input.setInputProcessor(new ProcesadorEntrada());
+    }
+
+    private void crearHUD() {
+        camaraHUD = new OrthographicCamera(ANCHO, ALTO);
+        camaraHUD.position.set(ANCHO/2, ALTO/2, 0);
+        camaraHUD.update();
+        vistaHUD = new StretchViewport(ANCHO, ALTO, camaraHUD);
     }
 
     private void crearMapa() {
@@ -66,6 +89,7 @@ public class PantallaMapas extends Pantalla
             actualizarCamara();
             quitarBloques(delta);
         }
+        actualizarTimer(delta);
 
         borrarPantalla(0,1,0);
 
@@ -74,8 +98,28 @@ public class PantallaMapas extends Pantalla
         rendererMapa.setView(camara);
         rendererMapa.render();
 
+        // INICIANDO
+        if (estado==EstadoJuego.INICIANDO) {
+            Gdx.app.log("INICIANDO", "Tiempo: " + (int)(timerPausa));
+        }
+
+        // USAR otra CÁMARA/VISTA
         if (estado == EstadoJuego.PAUSADO) {
+            batch.setProjectionMatrix(camaraHUD.combined);
             escenaPausa.draw();
+            batch.begin();
+            escenaPausa.sprite.draw(batch);
+            batch.end();
+
+            escenaPausa.sprite.rotate(5);
+        }
+    }
+
+    private void actualizarTimer(float delta) {
+        timerPausa += delta;
+        if (estado == EstadoJuego.INICIANDO && timerPausa>=3) {
+
+            estado = EstadoJuego.JUGANDO;
         }
     }
 
@@ -117,9 +161,10 @@ public class PantallaMapas extends Pantalla
             if (estado == EstadoJuego.JUGANDO) {
                 estado = EstadoJuego.PAUSADO;
                 // Crear escenaPausa
-                //if (escenaPausa==null) {
-                    escenaPausa = new EscenaPausa(vista, batch);
-                //}
+                if (escenaPausa==null) {
+                    escenaPausa = new EscenaPausa(vistaHUD, batch); // vistaHUD (no se mueve)
+                }
+                Gdx.input.setInputProcessor(escenaPausa);
                 Gdx.app.log("PAUSA", "Cambia a pausado....");
             } else if (estado == EstadoJuego.PAUSADO) {
                 estado = EstadoJuego.JUGANDO;
@@ -170,13 +215,20 @@ public class PantallaMapas extends Pantalla
     private enum EstadoJuego
     {
         JUGANDO,
-        PAUSADO
+        PAUSADO,
+        INICIANDO
     }
 
     private class EscenaPausa extends Stage
     {
+        Sprite sprite;
+
         public EscenaPausa(Viewport vista, SpriteBatch batch) {
             super(vista, batch);
+
+            Texture texturaSprite = new Texture("runner/bolaFuego.png");
+            sprite = new Sprite(texturaSprite);
+            sprite.setPosition(ANCHO/2, ALTO*0.6f);
 
             Pixmap pixmap = new Pixmap((int)(ANCHO*0.75f), (int)(0.8f*ALTO),
                     Pixmap.Format.RGBA8888);
@@ -186,10 +238,32 @@ public class PantallaMapas extends Pantalla
 
             Texture textura = new Texture(pixmap);
             Image imgPausa = new Image(textura);
-            imgPausa.setPosition(camara.position.x - pixmap.getWidth()/2,
+            imgPausa.setPosition(ANCHO/2 - pixmap.getWidth()/2,
                     ALTO/2 - pixmap.getHeight()/2);
 
-            this.addActor(imgPausa);
+            this.addActor(imgPausa);        // Fondo
+
+            // Botón(es)
+            // Botón de disparo
+            Texture texturaDisparo = new Texture("space/disparo.png");
+            TextureRegionDrawable region = new TextureRegionDrawable(new TextureRegion(
+                    texturaDisparo));
+
+            ImageButton btnDisparo = new ImageButton(region);
+            btnDisparo.setPosition(ANCHO/2, ALTO/3);
+            // Programar listener del botón
+            btnDisparo.addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    super.clicked(event, x, y);
+                    // QUITAR Pausa
+                    estado = EstadoJuego.JUGANDO;
+                    Gdx.app.log("PAUSA", "Reanuda por el botón de la pausa");
+                    Gdx.input.setInputProcessor(new ProcesadorEntrada());
+                }
+            });
+
+            this.addActor(btnDisparo);
         }
     }
 }
